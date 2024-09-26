62247d091dcad33469faa2f0f4ca1435ccc23391
f.janmohammadi <farshadjanmohammadi@gmail.com>
2024-09-26 17:51:12 +0330

    dynamic initial view

--- main.js
+++ main.js
@@ -1,37 +1,42 @@
-const box=document.querySelectorAll('.content');
+const box = document.querySelectorAll(".content");
 
-window.addEventListener('scroll',showBox);
+window.addEventListener("scroll", showBox);
 
+(function addInitialViewBox() {
+  const calculateCountBox = Math.trunc(window.innerHeight / box[0].clientHeight);
 
-function showBox(){
-        const heightWindow=window.innerHeight*0.8
+  console.log(calculateCountBox, "calculateCountBox");
 
+  box.forEach((element, index) => {
+    if (index > calculateCountBox - 1) return;
 
-        box.forEach(elemenet => {
-            const boxTop=elemenet.getBoundingClientRect().top
-            if (boxTop<heightWindow){
-                elemenet.classList.add('show');
-            }
-            else{
-                elemenet.classList.remove('show')
-            }
-        })
+    element.classList.add("show");
+  });
+})();
+
+function showBox() {
+  const heightWindow = window.innerHeight * 0.8;
+
+  box.forEach((elemenet) => {
+    const boxTop = elemenet.getBoundingClientRect().top;
+    if (boxTop < heightWindow) {
+      elemenet.classList.add("show");
+    } else {
+      elemenet.classList.remove("show");
+    }
+  });
 }
 // function showBox() {
-//     const heightWindow = window.innerHeight; 
+//     const heightWindow = window.innerHeight;
 //     const scrollTop = window.scrollY || document.documentElement.scrollTop;
 
 //     box.forEach(element => {
-//         const boxTop = element.offsetTop; 
+//         const boxTop = element.offsetTop;
 
 //         if (scrollTop + heightWindow > boxTop) {
-//             element.classList.add('show'); 
+//             element.classList.add('show');
 //         } else {
-//             element.classList.remove('show'); 
+//             element.classList.remove('show');
 //         }
 //     });
 // }
-
-
-
-
