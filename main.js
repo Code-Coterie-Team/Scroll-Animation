c491e7dd8747273914f225131b1e8215e35ddce4
f.janmohammadi <farshadjanmohammadi@gmail.com>
2024-09-26 17:36:55 +0330

    initial view and increase factor

--- main.js
+++ main.js
@@ -3,34 +3,34 @@ const box=document.querySelectorAll('.content');
 window.addEventListener('scroll',showBox);
 
 
-// function showBox(){
-//         const heightWindow=window.innerHeight*0.7
+function showBox(){
+        const heightWindow=window.innerHeight*0.8
 
 
-//         box.forEach(elemenet => {
-//             const boxTop=elemenet.getBoundingClientRect().top
-//             if (boxTop<heightWindow){
-//                 elemenet.classList.add('show');
-//             }
-//             else{
-//                 elemenet.classList.remove('show')
-//             }
-//         })
-// }
-function showBox() {
-    const heightWindow = window.innerHeight; 
-    const scrollTop = window.scrollY || document.documentElement.scrollTop;
-
-    box.forEach(element => {
-        const boxTop = element.offsetTop; 
-
-        if (scrollTop + heightWindow > boxTop) {
-            element.classList.add('show'); 
-        } else {
-            element.classList.remove('show'); 
-        }
-    });
+        box.forEach(elemenet => {
+            const boxTop=elemenet.getBoundingClientRect().top
+            if (boxTop<heightWindow){
+                elemenet.classList.add('show');
+            }
+            else{
+                elemenet.classList.remove('show')
+            }
+        })
 }
+// function showBox() {
+//     const heightWindow = window.innerHeight; 
+//     const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+//     box.forEach(element => {
+//         const boxTop = element.offsetTop; 
+
+//         if (scrollTop + heightWindow > boxTop) {
+//             element.classList.add('show'); 
+//         } else {
+//             element.classList.remove('show'); 
+//         }
+//     });
+// }
 
 
 
